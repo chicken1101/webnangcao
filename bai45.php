b05463bbf1097c72089b2589069233b6ca08250d
nguyentuananh <anhzzxx02@gmail.com>
2022-09-19 09:25:40 +0700

    PHP

---
+++ bai45.php
@@ -0,0 +1,12 @@
+<!-- Viết một chương trình PHP để tính tổng các chữ số của một số. -->
+<?php
+function sum_of_digits($nums) {
+    $digits_sum = 0;
+      for ($i = 0; $i < strlen($nums); $i++) {
+             $digits_sum += $nums[$i];
+               }
+      return $digits_sum;
+}
+echo sum_of_digits("12345")."\n";
+echo sum_of_digits("9999")."\n";
+?>
